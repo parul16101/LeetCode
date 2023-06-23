3319a8595e11e0f78eac1c37356cc43a7fc2743a
Parul Damahe <64355937+parul16101@users.noreply.github.com>
2023-06-23 09:54:15 -0600

    Added solution - LeetHub

---
+++ Count the paths - GFG/count-the-paths.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+
+    void dfs(int s, map<int,vector<int>> adj, int d, int &count)
+    {
+        if(s==d)
+            {
+                count+=1;
+                return;
+            }
+            
+        for(auto i:adj[s])
+        {
+            dfs(i,adj,d,count);
+        }
+    }
+    
+	int possible_paths(vector<vector<int>>edges, int n, int s, int d){
+	    
+	    int count = 0;
+	    
+	    map<int,vector<int>> adj;
+	    for(int i=0;i<edges.size();i++)
+	    {
+	        adj[edges[i][0]].push_back(edges[i][1]);
+	    }
+	    
+	    dfs(s,adj,d,count);
+	    
+	    return count;
+	    
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m, s, d;
+		cin >> n >> m >> s >> d;
+		vector<vector<int>>edges;
+		for(int i = 0; i < m; i++){
+			int u, v;
+			cin >> u >> v;
+			edges.push_back({u,v});
+		}
+		Solution obj;
+		int ans = obj.possible_paths(edges, n, s, d);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
