aa0ba42ca5f463446bed3903cefb6f81394608de
Parul Damahe <64355937+parul16101@users.noreply.github.com>
2023-08-18 17:31:12 -0500

    Added solution - LeetHub

--- Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
@@ -13,31 +13,25 @@ class Solution{
     int maxLen(vector<int>&A, int n)
     {   
         unordered_map<int,int> m;
-        int len = 0;
-        int ans = INT_MIN;
-        int pre_sum = 0;
-        
-        for(int i=0;i<n;i++)
-        {
-            pre_sum += A[i];
-            
-            if(pre_sum==0)
-            {
+       int maxi = INT_MIN;
+       int len = 0;
+       int presum = 0;
+       
+       for(int i=0;i<n;i++)
+       {
+           presum += A[i];
+           
+           if(presum==0)
                 len = i+1;
-                ans = max(ans,len);
-            }
-            
-            if(m.find(pre_sum)==m.end())
-                m[pre_sum] = i;
+            else if (m.find(presum)==m.end())
+                m[presum] = i;
             else
-                {
-                    len = i-m[pre_sum];
-                    ans = max(ans,len);
-                }
-        }
-        if(ans==INT_MIN)    
-            return 0;
-        return ans;
+                len = i-m[presum];
+                
+            maxi = max(maxi,len);
+       }
+       
+       return maxi;
     }
 };
 
