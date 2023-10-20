234ba6252554e2ec69bc19571af051d770138d21
Parul Damahe <64355937+parul16101@users.noreply.github.com>
2023-10-20 18:29:53 -0500

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -5,40 +5,37 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-    bool bipartite(int i, vector<int> &color, int c, vector<int> &vis,vector<int> adj[])
+    bool dfs(int i, vector<int> &vis, vector<int>adj[], int V, int c)
     {
-        vis[i] = 1;
-        color[i] = c;
+        vis[i] = c;
         c = c^1;
         
         for(auto j:adj[i])
         {
-            if(vis[j]==0)
+            if(vis[j]==-1)
             {
-                if(bipartite(j,color,c,vis,adj)==false)
+                if(dfs(j,vis,adj,V,c)==false)
 	                return false;
             }
-            else if(color[i]==color[j])
+            else if(vis[i]==vis[j])
                 return false;
         }
         return true;
     }
 
 	bool isBipartite(int V, vector<int>adj[]){
-	   
-	   vector<int> vis(V,0);
-	   vector<int> color(V,-1);
-	   int c = 0;
-	   
-	   for(int i=0;i<V;i++)
-	   {
-	       if(vis[i]==0)
-	       {
-	           if(bipartite(i,color,c,vis,adj)==false)
+	    vector<int> vis(V,-1);
+	    int c = 0;
+	    
+	    for(int i=0;i<V;i++)
+	    {
+	        if(vis[i]==-1)
+	        {
+	            if(dfs(i,vis,adj,V,c)==false)
 	                return false;
-	       }
-	   }
-	   return true;
+	        }
+	    }
+	    return true;
 	}
 
 };
