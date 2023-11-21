02fe3532907e43604493e76e968d0eb26aab7d36
Parul Damahe <64355937+parul16101@users.noreply.github.com>
2023-11-21 07:14:34 -0600

    Added solution - LeetHub

--- Bitonic Point - GFG/bitonic-point.cpp
+++ Bitonic Point - GFG/bitonic-point.cpp
@@ -20,7 +20,7 @@ public:
 	                return arr[mid];
 	        else if(arr[mid-1]<arr[mid] && arr[mid]<arr[mid+1])
 	                low = mid+1;
-	        else if(arr[mid-1]>arr[mid] && arr[mid]>arr[mid+1])
+	        else //if(arr[mid-1]>arr[mid] && arr[mid]>arr[mid+1])
 	                high = mid-1;
 	    }
 	    return 0;
